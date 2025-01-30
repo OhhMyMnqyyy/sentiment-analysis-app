56bc995b4f69192f7d562a84732639a108a7be45
AimanRifqy <93848455+OhhMyMnqyyy@users.noreply.github.com>
2025-01-30 18:14:06 +0800

    Update sentiment_model.py

--- sentiment_model.py
+++ sentiment_model.py
@@ -1,1 +1,15 @@
+pip install streamlit textblob pandas matplotlib
+
+from textblob import TextBlob
+
+def analyze_sentiment(review):
+    analysis = TextBlob(review)
+    polarity = analysis.sentiment.polarity
+    
+    if polarity > 0:
+        return "Positive 😊"
+    elif polarity < 0:
+        return "Negative 😡"
+    else:
+        return "Neutral 😐"
 
